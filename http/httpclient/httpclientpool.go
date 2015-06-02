51302b0908325bcabad94cdd337c22124b240378
nieanan [聂安安] <nieanan@xiaomi.com>
2015-06-02 11:28:08 +0800

    add unit test for httpclient, add timeout interface for httpclientpool

--- http/httpclient/httpclientpool.go
+++ http/httpclient/httpclientpool.go
@@ -6,38 +6,73 @@ import (
 	"time"
 )
 
-// TODO unit test
-// TODO support timeout in GetXXX()
-
+//
 var (
-	httpClientMap = nmap.NewSafeMap()
+	httpClientPool = NewHttpClientPool()
 )
 
-func Stop() {
-	CloseHttpClients()
+func GetHttpClient(name string, connTimeout time.Duration, reqTimeout time.Duration) *http.Client {
+	return httpClientPool.AddAndGetHttpClient(name, connTimeout, reqTimeout)
 }
 
-func GetHttpClient(name string) *http.Client {
-	hci, found := httpClientMap.Get(name)
-	if !found {
-		hci = newHttpClient(5, 20)
-		httpClientMap.Put(name, hci)
+func RemoveHttpClient(name string) {
+	httpClientPool.RemoveHttpClient(name)
+}
+
+// HttpClientPool Struct
+type HttpClientPool struct {
+	httpClientMap *nmap.SafeMap
+}
+
+func NewHttpClientPool() *HttpClientPool {
+	return &HttpClientPool{httpClientMap: nmap.NewSafeMap()}
+}
+
+func (hcp *HttpClientPool) AddHttpClient(name string, connTimeout time.Duration, reqTimeout time.Duration) *http.Client {
+	hci, found := hcp.httpClientMap.Get(name)
+	if found {
+		return hci.(*http.Client)
 	}
 
-	return hci.(*http.Client)
+	nhc := hcp.newHttpClient(connTimeout, reqTimeout)
+	hcp.httpClientMap.Put(name, nhc)
+	return nhc
 }
 
-func CloseHttpClient(name string) {
-	if client, found := httpClientMap.Get(name); found {
+func (hcp *HttpClientPool) GetHttpClient(name string) (*http.Client, bool) {
+	hci, found := hcp.httpClientMap.Get(name)
+	if found {
+		return hci.(*http.Client), true
+	}
+	return &http.Client{}, false
+}
+
+func (hcp *HttpClientPool) AddAndGetHttpClient(name string, connTimeout time.Duration, reqTimeout time.Duration) *http.Client {
+	hci, found := hcp.httpClientMap.Get(name)
+	if found {
+		return hci.(*http.Client)
+	}
+
+	nhc := hcp.newHttpClient(connTimeout, reqTimeout)
+	hcp.httpClientMap.Put(name, nhc)
+	return nhc
+}
+
+func (hcp *HttpClientPool) Size() int {
+	return hcp.httpClientMap.Size()
+}
+
+func (hcp *HttpClientPool) RemoveHttpClient(name string) {
+	if client, found := hcp.httpClientMap.GetAndRemove(name); found {
 		if client.(*http.Client).Transport != nil {
 			client.(*http.Client).Transport.(*Transport).Close()
 		}
 	}
 }
 
-func CloseHttpClients() {
-	for _, key := range httpClientMap.Keys() {
-		clienti, found := httpClientMap.Get(key)
+func (hcp *HttpClientPool) RemoveAllHttpClients() {
+	for _, key := range hcp.httpClientMap.Keys() {
+		clienti, found := hcp.httpClientMap.GetAndRemove(key)
 		if found {
 			client := clienti.(*http.Client)
 			if client.Transport != nil {
@@ -48,10 +83,10 @@ func CloseHttpClients() {
 }
 
 // internal
-func newHttpClient(connTimeout int, reqTimeout int) *http.Client {
+func (hcp *HttpClientPool) newHttpClient(connTimeout time.Duration, reqTimeout time.Duration) *http.Client {
 	transport := &Transport{
-		ConnectTimeout: time.Duration(connTimeout) * time.Second,
-		RequestTimeout: time.Duration(reqTimeout) * time.Second,
+		ConnectTimeout: connTimeout,
+		RequestTimeout: reqTimeout,
 	}
 	return &http.Client{Transport: transport}
 }
